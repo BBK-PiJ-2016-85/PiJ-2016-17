9daccbf990b5a3aad2a06fdc5ec804c3dcdfd6ca
Keith Mannock <keith@dcs.bbk.ac.uk>
2017-01-14 13:54:55 +0000

    exercises/Day05

---
+++ exercises/Day05/src/ex02/myScript.groovy
@@ -0,0 +1,19 @@
+package ex02
+
+ArraySource source = new ArraySource()
+int[] symetricalArray = source.symetricalArray 
+int[] nonSymetricalArray = source.nonSymetricalArray 
+
+ArrayChecker myChecker = new ArrayChecker()
+print "First array: "
+int[] arrayToPrint = myChecker.checkAndReverse(symetricalArray)
+myChecker.printArray(arrayToPrint)
+print "Second array: " 
+arrayToPrint = myChecker.checkAndReverse(nonSymetricalArray)
+myChecker.printArray(arrayToPrint)
+
+
+
+
+
+
